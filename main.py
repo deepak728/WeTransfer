8b0d63d2a6dea1d43f6d3ee2ed7f72733319d554
Deepak Verma <getdeepak98@gmail.com>
2017-02-05 10:53:59 +0530

    fetch user details

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
 import json, requests 
+from config import accesstoken
 app = Flask(__name__)
- 
+token=accesstoken().assign_token()
+
 @app.route("/")
 def login():
     return render_template('index.html')
 
+@app.route("/take")
+def home():
+	r=requests.get("https://api.onedrive.com/v1.0/drive",
+		headers = {'Content-Type': 'application/json'},
+		params={'access_token':token})
+	rstr= str(r.text)
+	rjson=json.loads(rstr)
+	return rjson['id']
 if __name__ == "__main__":
     app.run(debug=True)
