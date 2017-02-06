e3a7b80850442b95f2c2e990b065051511d8fe49
Deepak Verma <getdeepak98@gmail.com>
2017-02-07 04:18:22 +0530

    added oauth2.0 authentication to generate token

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
 import json, requests 
 import mysql.connector
-from config import accesstoken,dbconnect
+from config import dbconnect
+import os
 app = Flask(__name__)
-token=accesstoken().assign_token()
 db_config=dbconnect().db_config()
 
 @app.route("/")
-def login():
+def index():
     return render_template('index.html')
 
+@app.route("/callback")
+def abc():
+    return render_template('call.html')    
+
 @app.route("/login")
-def home():
+def login():
+	token = request.cookies.get('odauth')
 	r=requests.get("https://api.onedrive.com/v1.0/drive",
 		headers = {'Content-Type': 'application/json'},
 		params={'access_token':token})
+	
 	rstr= str(r.text)
 	rjson=json.loads(rstr)
 	userdata = {
@@ -34,18 +40,32 @@ def home():
 	rjson=json.loads(rstr)
 	length=len(rjson['value'])
 	for i in range(0,length):
+		re=requests.get("https://api.onedrive.com/v1.0/drive/items/"+rjson['value'][i]['id']+"/children?select=id,name&expand=thumbnails(select=large)",
+		headers = {'Content-Type': 'application/json'},
+		params={'access_token':token})
+		print(re.text)	
 		filedata={
 			'driveid': rjson['value'][i]['createdBy']['user']['id'],
 			'id':rjson['value'][i]['id'],
 			'filename':rjson['value'][i]['name'],
-			'size': rjson['value'][i]['size']
+			'size': 'Size:' + str(rjson['value'][i]['size']/1024)+'MB',
+			'downloadurl':rjson['value'][i]['@content.downloadUrl']
 		}
-		add_files = ("INSERT INTO files (driveid,id,filename,size) VALUES (%(driveid)s, %(id)s, %(filename)s, %(size)s)")
+
+		add_files = ("INSERT INTO files (driveid,id,filename,size,downloadurl) VALUES (%(driveid)s, %(id)s, %(filename)s, %(size)s,%(downloadurl)s)")
 		cursor.execute(add_files, filedata)
 	db.commit()
-	cursor.close()
-	db.close()
-	return render_template('home.html')
+	cursor.execute("SELECT * FROM files")
+	data = cursor.fetchall()
+	return render_template('home.html',data=data)
+	
+@app.route("/home")
+def home():
+	db = mysql.connector.connect(host=db_config['host'],database=db_config['database'],user=db_config['user'],password=db_config['password'])
+	cursor = db.cursor()
+	cursor.execute("SELECT * FROM files")
+	data = cursor.fetchall()
+	return render_template('home.html',data=data)
 
 
 	
